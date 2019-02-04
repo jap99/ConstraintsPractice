67c05984ba51f4c5fe871fc1a638db327807b5ff
javidpoornasir <javidp.sf@gmail.com>
2019-02-04 15:56:23 -0800

    first commit

---
+++ NewProject/MyCustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyCustomCell.swift
+//  NewProject
+//
+//  Created by Javid Poornasir on 2/4/19.
+//  Copyright © 2019 jp. All rights reserved.
+//
+
+import UIKit
+
+class MyCustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
